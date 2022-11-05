084c6bd9e69f9866b3c2b7fde3038c21191c54a0
Jenniffer Melchiade <jdenisemelch@gmail.com>
2022-11-05 17:25:38 -0400

    added main and hero components

---
+++ src/components/Main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+
+const Main = () => {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+    </div>
+  );
+};
+
+export default Main;
--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,3 @@
 export { default as Navbar } from "./Navbar";
+export { default as hero } from "./hero";
+export { default as Main } from "./Main";
